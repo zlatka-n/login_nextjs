dfd45313fe72fde9b2989fbc66665d580025ae39
zlatka-n <zla.b.ngu@gmail.com>
2021-12-25 09:06:16 +0100

    sign in test
    new message

---
+++ components/SignIn.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SignIn from "../components/SignIn";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import AuthProvider from "../context/AuthContext";
+
+test.skip("on initial render, the  button is disabled", () => {
+  render(<SignIn />, { wrapper: AuthProvider });
+  // expect(await screen.findByRole("button", { name: /Login/i })).toBeDisabled();
+});
