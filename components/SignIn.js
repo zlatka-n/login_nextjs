454fa4f5292f3763bdf4a08dda649febfd36590c
zlatka-n <zla.b.ngu@gmail.com>
2022-02-02 08:00:29 +0100

    old import deleted
    squash combining

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,6 +1,6 @@
-import SignForm from "./SignForm";
 import withSign from "./SignHOC";
 
+///squash 1
 function SignIn({ form }) {
   return <div>{form}</div>;
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import SignIn from "../components/SignIn";
 import Dashboard from "./dashboard";
 import AuthContext from "@/context/AuthContext";
+///squash 2
 
 export default function Home() {
   const { user } = useContext(AuthContext);
