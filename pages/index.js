a7a3c3d1057903a24c655b7b0ea1308b4633ecfc
zlatka-n <zla.b.ngu@gmail.com>
2022-02-03 07:52:18 +0100

    f2

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import SignIn from "../components/SignIn";
 import Dashboard from "./dashboard";
 import AuthContext from "@/context/AuthContext";
 ///squash 2
-
+//f2
 export default function Home() {
   const { user } = useContext(AuthContext);
   const title = "Sign In";
