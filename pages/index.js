737334ebe9f63e9b085810343983cad8fd503d68
zlatka-n <zla.b.ngu@gmail.com>
2022-02-03 07:53:50 +0100

    hf1

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import SignIn from "../components/SignIn";
 import Dashboard from "./dashboard";
 import AuthContext from "@/context/AuthContext";
 
+//hf1
+
 export default function Home() {
   const { user } = useContext(AuthContext);
   return <div>{user ? <Dashboard /> : <SignIn />}</div>;
